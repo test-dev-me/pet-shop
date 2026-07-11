397ce33675e516bba9cef93ec8ca32759ae4843d
Ali <akhavan.dev@gmail.com>
2026-07-11 18:30:06 +0330

    Revert "new commit"
    This reverts commit 5f1c9f75e5a3b2fd24036ba43b824b87afeddad0.

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,12 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
 function Header() {
-  const [counter, setCounter] = useState(0);
-
   return (
     <header className="text-lg bg-sky-700 text-white text-center py-10 mb-5">
-      <button onClick={() => setCounter((c) => c + 1)}>Increase</button>
-      <img src="" alt="logo" />
+      Header
     </header>
   );
 }
